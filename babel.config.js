549ff1d32cc2269effd7ae456b86bf03ddee2aee
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-08-23 11:06:50 -0300

    path  alias

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,26 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          alias: {
+            "@dtos": "./src/dtos",
+            "@components": "./src/components",
+            "@storage": "./src/storage",
+            "@utils": "./src/utils",
+            "@screens": "./src/screens",
+            "@assets": "./src/assets",
+            "@hooks": "./src/hooks",
+            "@context": "./src/context",
+            "@routes": "./src/routes",
+            "@services": "./src/services",
+          },
+        },
+      ],
+    ],
   };
 };
