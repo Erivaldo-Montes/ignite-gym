e6880ed52b729b6c7a839bcb475308c6bc8dc886
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-08-29 17:10:31 -0300

    header

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,24 @@
+import { HStack, Heading, Text, VStack } from "native-base";
+import { UserPhoto } from "./UserPhoto";
+export function HomeHeader() {
+  return (
+    <HStack bg={"gray.600"} pt={16} pb={6} px={8} alignItems={"center"}>
+      <UserPhoto
+        size={10}
+        source={{
+          uri: "https://github.com/Erivaldo-Montes.png",
+        }}
+        alt="Imagem do usuário"
+        mr={4}
+      />
+      <VStack>
+        <Text color={"gray.100"} fontSize={"md"}>
+          Olá,
+        </Text>
+        <Heading color={"gray.100"} fontSize={"md"}>
+          Erivaldo
+        </Heading>
+      </VStack>
+    </HStack>
+  );
+}
