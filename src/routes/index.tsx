c446618a3f8a9d6a60ba979ba0cb3c5df192dcf0
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-08-30 15:15:29 -0300

    app

---
+++ src/routes/index.tsx
@@ -0,0 +1,19 @@
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import { useTheme, Box } from "native-base";
+
+import { AppRoutes } from "./app.routes";
+import { AuthRoute } from "./auth.route";
+
+export function Routes() {
+  const { colors } = useTheme();
+
+  const THEME = DefaultTheme;
+  THEME.colors.background = colors.gray["700"];
+  return (
+    <Box flex={1} bg={"gray.700"}>
+      <NavigationContainer theme={THEME}>
+        <AppRoutes />
+      </NavigationContainer>
+    </Box>
+  );
+}
