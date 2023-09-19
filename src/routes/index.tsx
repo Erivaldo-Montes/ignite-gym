0593b0bf527d41cb83f842aec1a0560de8e04426
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-09-18 21:35:08 -0300

    loading screen while open app

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,15 +3,21 @@ import { useTheme, Box } from "native-base";
 
 import { AppRoutes } from "./app.routes";
 import { AuthRoute } from "./auth.route";
+import { Loading } from "@components/Loading";
 
 import { useAuth } from "@hooks/useAuth";
 
 export function Routes() {
   const { colors } = useTheme();
-  const { user } = useAuth();
+  const { user, isLoadingUserStorageData } = useAuth();
 
   const THEME = DefaultTheme;
   THEME.colors.background = colors.gray["700"];
+
+  if (isLoadingUserStorageData) {
+    return <Loading />;
+  }
+
   return (
     <Box flex={1} bg={"gray.700"}>
       <NavigationContainer theme={THEME}>
