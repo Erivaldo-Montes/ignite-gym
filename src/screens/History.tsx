3bff5d73b9f3e16f3faa02e2888fb5dbea1a1a74
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-09-28 15:50:53 -0300

    get history exercise

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,21 +1,49 @@
-import { useState } from "react";
-import { VStack, Text, Heading, SectionList } from "native-base";
+import { useState, useCallback } from "react";
+import { VStack, Text, Heading, SectionList, useToast } from "native-base";
+import { useFocusEffect } from "@react-navigation/native";
 
+import { api } from "@services/api";
 import { HistoryCard } from "@components/HistoryCard";
 import { ScreenHeader } from "@components/ScreenHeader";
+import { AppError } from "@utils/AppError";
+import { HistoryByDayDTO } from "@DTOs/HistoryByDayDTO";
 
 export function History() {
-  const [historyExercise, setHistoryExercise] = useState([
-    { title: "26.07.2024", data: ["Costas", "Puxada Frontal"] },
-    { title: "27.07.2024", data: ["Puxada Frontal"] },
-  ]);
+  const [historyExercise, setHistoryExercise] = useState<HistoryByDayDTO[]>([]);
+  const toast = useToast();
+  const [isLoading, setIsLoading] = useState(true);
+
+  async function fetchHistory() {
+    try {
+      setIsLoading(true);
+      const response = await api.get("/history");
+      setHistoryExercise(response.data);
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError
+        ? error.message
+        : "Não foi possível obter o histórico";
+
+      toast.show({
+        title,
+        placement: "top",
+        bgColor: "red.500",
+      });
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchHistory();
+    }, [])
+  );
   return (
     <VStack flex={1}>
       <ScreenHeader title="Histórico de exercícios" />
       <SectionList
         sections={historyExercise}
-        keyExtractor={(item) => item}
-        renderItem={({ item }) => <HistoryCard />}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <HistoryCard data={item} />}
         renderSectionHeader={({ section }) => (
           <Heading
             color={"gray.200"}
